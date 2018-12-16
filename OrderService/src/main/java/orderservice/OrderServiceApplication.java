15bc454af3c7d052a84bf1f70e47b5a843a9ab16
Andrea Cifola <cifola.andrea@gmail.com>
2018-12-16 11:39:48 +0100

    First Commit

---
+++ OrderService/src/main/java/orderservice/OrderServiceApplication.java
@@ -0,0 +1,14 @@
+package orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EntityScan(basePackages = {"the package on which it depends"}) //Add this only if it realy depends on the SagaOrchestrator module
+//@ComponentScan(basePackages = {"the package on which it depends"}) //Add this only if it realy depends on the SagaOrchestrator module
+public class  OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class);
+    }
+}
